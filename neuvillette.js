0da7809eefcb15a4ee3231b319733704157a6e0b
Ziggismalls <frantz.o@icloud.com>
2025-01-23 15:42:32 +0100

    story raiden a jour + animations + gifs

--- neuvillette.js
+++ neuvillette.js
@@ -104,7 +104,7 @@ const storyDataNeuvillette = [
       },
       {
         text: "Le confronter immédiatement pour éviter tout conflit futur",
-        nextQuestion: 16,
+        nextQuestion: 15,
       },
     ],
   },
@@ -114,11 +114,11 @@ const storyDataNeuvillette = [
     choices: [
       {
         text: "Mettre en place un plan d'urgence pour prévenir les inondations",
-        nextQuestion: 17,
+        nextQuestion: 15,
       },
       {
         text: "Chercher une solution magique plus audacieuse",
-        nextQuestion: 18,
+        nextQuestion: 15,
       },
     ],
   },
@@ -128,11 +128,11 @@ const storyDataNeuvillette = [
     choices: [
       {
         text: "Étudier l'artefact et l'utiliser pour protéger la ville",
-        nextQuestion: 19,
+        nextQuestion: 15,
       },
       {
         text: "Sceller l'artefact pour éviter toute catastrophe",
-        nextQuestion: 20,
+        nextQuestion: 15,
       },
     ],
   },
@@ -142,11 +142,11 @@ const storyDataNeuvillette = [
     choices: [
       {
         text: "Utiliser la force pour réprimer la rébellion",
-        nextQuestion: 21,
+        nextQuestion: 15,
       },
       {
         text: "Négocier avec les rebelles pour apaiser la situation",
-        nextQuestion: 22,
+        nextQuestion: 15,
       },
     ],
   },
@@ -156,11 +156,11 @@ const storyDataNeuvillette = [
     choices: [
       {
         text: "Utiliser l'artefact Hydro pour stopper l'inondation",
-        nextQuestion: 23,
+        nextQuestion: 15,
       },
       {
         text: "Chercher une solution plus traditionnelle avec les ingénieurs",
-        nextQuestion: 24,
+        nextQuestion: 15,
       },
     ],
   },
@@ -170,11 +170,11 @@ const storyDataNeuvillette = [
     choices: [
       {
         text: "Enquêter et éliminer les conspirateurs discrètement",
-        nextQuestion: 25,
+        nextQuestion: 15,
       },
       {
         text: "Informer le public et agir de manière transparente",
-        nextQuestion: 26,
+        nextQuestion: 15,
       },
     ],
   },
@@ -184,11 +184,11 @@ const storyDataNeuvillette = [
     choices: [
       {
         text: "Accepter leur aide pour renforcer l'efficacité de la gestion",
-        nextQuestion: 27,
+        nextQuestion: 15,
       },
       {
         text: "Refuser leur aide pour préserver son autorité",
-        nextQuestion: 28,
+        nextQuestion: 15,
       },
     ],
   },
@@ -198,9 +198,9 @@ const storyDataNeuvillette = [
     choices: [
       {
         text: "Tenter de contrôler l'artefact pour son propre usage",
-        nextQuestion: 29,
+        nextQuestion: 15,
       },
-      { text: "Le détruire pour éviter tout danger", nextQuestion: 30 },
+      { text: "Le détruire pour éviter tout danger", nextQuestion: 15 },
     ],
   },
   {
@@ -212,73 +212,248 @@ const storyDataNeuvillette = [
 const storyDataRaiden = [
   {
     question:
-      "Oh oh oh Matelo, je suis Gangplank le roi des pillards déchu. Cela fait maintenant 3 ans que je suis poursuivi par le Grand général Noxus pour avoir dérobé le Léviathan, et j'aurai besoin de ton aide pour un mission assez délicate. Serai-tu prêt à m'aider ?",
+      "Raiden reçoit une lettre mystérieuse de quelqu'un se disant prisonnier à Meropide, clamant une menace imminente.",
     choices: [
       {
-        text: "Prendre du temps pour réfléchir",
-        nextQuestion: 6,
+        text: "Décoder immédiatement la lettre pour en apprendre plus",
+        nextQuestion: 2,
       },
       {
-        text: "Ecouter la suite.",
+        text: "Demander a Corinde de vous aider à décrypter le message",
         nextQuestion: 1,
+      },
+      {
+        text: "Ignorer la lettre et la considerer comme un diversion",
+        nextQuestion: "",
       },
     ],
   },
   {
     question:
-      "Il faut que tu partes vers l'Ouest, un bateau t'attend au port de la ville. Il s'en va dans 3h prépare ta valisoche, un bon K-way ainsi qu'une bonne douzaine de boite de fricandelles. J'aimerai que tu passe également à la bibiliothèque de mestre Emond pour récuperer la carte de l'île de Tougage. Un trésor y est enfouis depuis des milliers d'années.",
+      "Vous découvre que le prisionnier mentionné est un certait 'Ezarn' ",
     choices: [
       {
-        text: "Lui répondre que vous y allez de ce pas.",
-        nextQuestion: 2,
+        text: "Interroger Ezarn directement dans sa cellule",
+        nextQuestion: 3,
       },
       {
-        text: "Demander plus d'information sur le Talisman",
+        text: "Consulter les archives pour vérifier le passé d'Ezarn",
+        nextQuestion: 2,
+      },
+    ],
+  },
+  {
+    question:
+      "Ezarn est un ancien chercher de fontaine, arrêté pour espionnage",
+
+    choices: [
+      {
+        text: "Partir interroger Ezarn",
         nextQuestion: 3,
       },
     ],
   },
   {
     question:
-      "Avant que tu t'en ailles, il faut que je te préviennes. Mestre Emond est un homme sage, mais il n'accord ca confiance qu'à ceux qu'il juge digne de la recevoir.",
+      " Ezarn révèle que son travail impliquait une expérience sur un 'coeur abyssal' et qu'un groupe de mercenaire cherche à s'en emparer",
     choices: [
       {
-        text: "Lui demander comment gagner la confiance du mestre",
+        text: "Creuser plus profondémment pour comprendre le lien entre Meropide et cet expérience",
         nextQuestion: 4,
       },
-    ],
-  },
-  {
-    question:
-      "Le talisman de l'île de Tougato, également connu sous le nom de'Cœur de l’Éther', est un artefact ancien et puissant, dont les origines remontent à une époque bien avant l’ère des civilisations connues. Selon les légendes transmises par les anciens sages, ce talisman serait une relique d’une civilisation disparue, les Aériens, qui régnaient autrefois sur des îles flottantes, invisibles aux yeux des mortels. Leur pouvoir venait de l'harmonie qu'ils avaient avec les énergies naturelles du monde, et le talisman en était le catalyseur.",
-    choices: [
       {
-        text: "Lui répondre que vous y allez de ce pas",
-        nextQuestion: 2,
-      },
-      {
-        text: "Dire que vous ne voulez pas risquez votre vie pour si peu et retourner à vos occupations",
-        nextQuestion: 6,
-      },
-    ],
-  },
-  {
-    question:
-      "Parle lui d'ouvrages anciens du XVe siècle, il deviendra fou et se mettra a boire du vin, BEAUCOUP de vin. Ainsi il sera plus facile à amadouer et verra en toi un nouvel ami avec qui il pourra partager sa plus grande passion pour la lecture d'encyclopédie.",
-    choices: [
-      {
-        text: "Lui demander ou se situe la bibliothèque",
+        text: " Emettre un avis d'alerte pour renforcer la sécurité de la forteresse",
         nextQuestion: 5,
       },
     ],
   },
   {
     question:
-      "Elle se trouve à quelques lieux d'ici dans le domaine de Valonia, il te faudra marcher 10min jusqu'a la tour des ordres de la ville, puis tu entre dans la tour du conseil, en bas il y aura une cave. Tu rentres dans la cave et tu parles a Serge le Mytho, il te donnera une monture pour que tu puisse t'y rendre plus rapidement. Si tu n'as pas d'or, ramène lui 3 sauges et une brindille de shokkoth comme monnaie de paiement.",
+      "il vous explique qu'un agent infiltré au sein de la forteresse tente de saboter les dispositifs de sécurité",
+
     choices: [
       {
-        text: "Le remercier et partir à l'aventure.",
+        text: "Croire Ezarn et emettre un avis d'alerte pour renforcer la sécurité de Meropide",
+        nextQuestion: 5,
+      },
+      {
+        text: "Traquez immediatement le saboteur avec l'aide des gardes",
+        nextQuestion: 5,
+      },
+      {
+        text: "laisser le saboteur en liberté pour suivre ses mouvements",
+        nextQuestion: 5,
+      },
+    ],
+  },
+  {
+    question: "Vous avez retrouvez le saboteur, que decidez vous ?",
+
+    choices: [
+      {
+        text: "Le menacer de le balancer dans la fosse au requins",
         nextQuestion: 6,
+      },
+      {
+        text: "Appelez des renforts ",
+        nextQuestion: 7,
+      },
+      {
+        text: " Soutirer des informations au saboteur",
+        nextQuestion: 8,
+      },
+    ],
+  },
+  {
+    question:
+      "Le saboteur n'ayant pas envie de finir en boullie se livre a vous",
+    choices: [
+      {
+        text: "Ecoutez ses paroles",
+        nextQuestion: 8,
+      },
+    ],
+  },
+  {
+    question:
+      "Le saboteur désormais entouré de la garde de la forteresse n'as plus le choix que de donner des infos",
+    choices: [
+      {
+        text: "Ecoutez ses paroles",
+        nextQuestion: 8,
+      },
+    ],
+  },
+  {
+    question:
+      "Le saboteur révèle qu'il agit pour le compte d'un groupe appelé les 'Echos de l'Abysse' ",
+    choices: [
+      {
+        text: "Infiltrer leurs cachette grâce aux informations du saboteur",
+        nextQuestion: 9,
+      },
+      {
+        text: " Préparer une attaque préventive contre leurs base ",
+        nextQuestion: 10,
+      },
+      {
+        text: " Contacter les chevaliers de Fontaine pour demander des renforts",
+        nextQuestion: 11,
+      },
+    ],
+  },
+  {
+    question: " Vous êtes enfin arrivé dans la base ennemi",
+    choices: [
+      {
+        text: "Saboter leurs plans en détruisant leurs équipements",
+        nextQuestion: 12,
+      },
+      {
+        text: " Capturer un des lieutenants pour obtenir des informations critique",
+        nextQuestion: 13,
+      },
+    ],
+  },
+  {
+    question:
+      "Les Echos de l'Abysse se préparent à voler le 'coeur abyssal', une source de puissance destructrice",
+    choices: [
+      {
+        text: "Affronter directement leurs chef",
+        nextQuestion: 14,
+      },
+    ],
+  },
+  {
+    question:
+      " Les Chevaliers vous répondent qu'ils sont tous parti au Sud et qu'il ne sauront pas la à temps, il vous faut donc agir seul",
+    choices: [
+      {
+        text: "Prendre les choses en main",
+        nextQuestion: 10,
+      },
+    ],
+  },
+  {
+    question:
+      " Felicitation, vous avez Réussit à repousser l'attaque de l'Echo de l'Abysse !",
+    choices: [
+      {
+        text: "Retourner à la forteresse",
+        nextQuestion: 19,
+      },
+    ],
+  },
+  {
+    question:
+      "Le chef des Echos apparait devant vous, prêt a sacrifier son lieutnant pour récuperer le coeur.",
+    choices: [
+      {
+        text: " L'affronter directement pour mettre fin à la menace",
+        nextQuestion: 14,
+      },
+      {
+        text: "Negocier une trêve pour épargner les innocents",
+        nextQuestion: 15,
+      },
+      {
+        text: "Détruire le coeur abyssal sur place, quoi qu'il en coute",
+        nextQuestion: 17,
+      },
+    ],
+  },
+  {
+    question:
+      "Après plusieurs heures de combats, vous arrivez finalement à vaincre le Chef de l'Echo de l'Abysse",
+    choices: [
+      {
+        text: "Retourner a Meropide",
+        nextQuestion: 19,
+      },
+    ],
+  },
+  {
+    question:
+      "Le chef ne souhaitant pas abandonner la chasse pour le coeur abyssal, vous propose un marché. Il vous demande en échange que vous lui donnez votre précieuse épée. ",
+    choices: [
+      {
+        text: " J'accepte ",
+        nextQuestion: 16,
+      },
+      {
+        text: "Je refuse ( cela va automatiquement vous faire combattre le Chef de l'Echo de l'Abysse) ",
+        nextQuestion: 14,
+      },
+    ],
+  },
+  {
+    question:
+      " Vous avez certes perdu votre épée mais avez sauvé la vie de centaines, voir de millers d'innocent. Votre patience et votre détermination on fait que vous avez gérez au mieux la situation sans en venir au combat. ",
+    choices: [
+      {
+        text: " Retourner à Meropide",
+        nextQuestion: 19,
+      },
+    ],
+  },
+  {
+    question:
+      "Malgré votre Ultime, vous n'avez pas réussit à détruire le coeur... Vous n'avez plus le choix, fuire ou vous battre. Qu'allez vous faire ?",
+    choices: [
+      {
+        text: "Se défendre contre le Lieutnant et le Chef ",
+        nextQuestion: 18,
+      },
+    ],
+  },
+  {
+    question:
+      " Après plusieurs heures de combats, vous arrivez finalement à vaincre le lieutnant ainsi que Chef de l'Echo de l'Abysse ",
+    choices: [
+      {
+        text: "Rentrer à Méropide",
+        nextQuestion: 19,
       },
     ],
   },
